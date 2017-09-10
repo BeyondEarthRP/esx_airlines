a5c5b44efcf0acf40ab641d1c53f13f2d2fd81a6
loko06320 <montet.steve@gmail.com>
2017-09-10 04:36:46 +0200

    Add files via upload

---
+++ de_esx_airlines_full.sql
@@ -0,0 +1,21 @@
+USE `essentialmode`;
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+  ('society_airlines','Airlines',1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+  ('society_airlines','Airlines',1)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+  ('airlines','Airlines')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+  ('airlines',0,'recrue','Rekrut',12,'{}','{}'),
+  ('airlines',1,'chauffeur','Anfänger',24,'{}','{}'),
+  ('airlines',2,'pilote','Experte',36,'{}','{}'),
+  ('airlines',3,'gerant','Schichtführer',48,'{}','{}'),
+  ('airlines',4,'boss','Chef',0,'{}','{}')
+;
---
+++ esx_airlines_full.sql
@@ -0,0 +1,21 @@
+USE `essentialmode`;
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+  ('society_airlines','Airlines',1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+  ('society_airlines','Airlines',1)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+  ('airlines','Airlines')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+  ('taxi',0,'recrue','Recrue',12,'{}','{}'),
+  ('taxi',1,'chauffeur','Novice',24,'{}','{}'),
+  ('taxi',2,'pilote','Experimente',36,'{}','{}'),
+  ('taxi',3,'gerant','Uber',48,'{}','{}'),
+  ('taxi',4,'boss','Patron',0,'{}','{}')
+;
