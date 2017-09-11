b608ddbaf060997be360196fcbc1161b855503dd
loko06320 <montet.steve@gmail.com>
2017-09-11 03:30:56 +0200

    Update esx_airlines.sql

--- esx_airlines.sql
+++ esx_airlines.sql
@@ -14,8 +14,8 @@ INSERT INTO `jobs` (name, label) VALUES
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
   ('taxi',0,'recrue','Recrue',12,'{}','{}'),
-  ('taxi',1,'chauffeur','Novice',24,'{}','{}'),
-  ('taxi',2,'pilote','Experimente',36,'{}','{}'),
-  ('taxi',3,'gerant','Uber',48,'{}','{}'),
+  ('taxi',1,'chauffeur','Chauffeur',24,'{}','{}'),
+  ('taxi',2,'pilote','Pilote',36,'{}','{}'),
+  ('taxi',3,'gerant','Gerant',48,'{}','{}'),
   ('taxi',4,'boss','Patron',0,'{}','{}')
 ;
