6d63818558e95fbcf317e7dd40c127b612c49766
loko06320 <montet.steve@gmail.com>
2017-09-11 03:47:07 +0200

    Add files via upload

--- localization/esx_airlines.sql
+++ localization/esx_airlines.sql
@@ -1,5 +1,9 @@
 USE `essentialmode`;
 
+INSERT INTO `addon_account` (name, label, shared) VALUES
+  ('society_airlines','Airlines',1)
+;
+
 INSERT INTO `addon_inventory` (name, label, shared) VALUES
   ('society_airlines','Airlines',1)
 ;
@@ -10,8 +14,8 @@ INSERT INTO `jobs` (name, label) VALUES
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
   ('taxi',0,'recrue','Recrue',12,'{}','{}'),
-  ('taxi',1,'chauffeur','Novice',24,'{}','{}'),
-  ('taxi',2,'pilote','Experimente',36,'{}','{}'),
-  ('taxi',3,'gerant','Uber',48,'{}','{}'),
+  ('taxi',1,'chauffeur','Chauffeur',24,'{}','{}'),
+  ('taxi',2,'pilote','Pilote',36,'{}','{}'),
+  ('taxi',3,'gerant','Gerant',48,'{}','{}'),
   ('taxi',4,'boss','Patron',0,'{}','{}')
 ;
